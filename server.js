b8ab3d5264a6faad07ed5274bc64554cd1492d85
unknown <rafaelmedeirospb@hotmail.com>
2020-05-30 15:41:30 -0300

    last

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.get('/', function(req, res){
+    res.redirect('/kanban');
+ });
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
